82f36c7274c4e3341b2c83a016fc90530e4f0aba
ВашеИмя <your_email@example.com>
2026-09-08 14:10:54 +0300

    Создал модель Category в приложении catalog.

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
 
-    name = models.CharField(max_length=50, verbose_name = 'Наименование')
+    name = models.CharField(max_length=100, verbose_name = 'Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(verbose_name='Изображение', blank=True, null=True)
     category = models.CharField(max_length=50, verbose_name='Категория')
@@ -18,4 +18,15 @@ class Product(models.Model):
 
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        ordering = ['name']
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    class Meta:
+
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
         ordering = ['name']
